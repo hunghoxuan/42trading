40ec7169335211cd20d52eaedf811b7406c812a2
Hung Ho <classondev@gmail.com>
2026-05-14 21:00:07 +0200

    chore(version): align cTrader build version with current deploy

--- bridge-clients/TVBridge_CTrader.cs
+++ bridge-clients/TVBridge_CTrader.cs
@@ -59,7 +59,7 @@ namespace cAlgo.Robots
         [Parameter("Trailing Step (Pips)", Group = "Automation", DefaultValue = 5, MinValue = 1)]
         public double Trail_Step { get; set; }
 
-        private const string BuildVersion = "v2026.05.14 20:45 - rr-risk-reward-mapping-fix";
+        private const string BuildVersion = "v2026.05.14 18:58 - 39df8d4c";
 
         private string _serverStatus = "WAITING";
         private string _apiStatus = "WAITING";
