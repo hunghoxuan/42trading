20fe57d1a678cbdf5bc631895876f0e615a293b8
Hung Ho <classondev@gmail.com>
2026-05-21 20:27:39 +0200

    codex: chore bump build versions for direct-setposition revert

--- bridge-clients/TVBridge_CTrader.cs
+++ bridge-clients/TVBridge_CTrader.cs
@@ -68,7 +68,7 @@ namespace cAlgo.Robots
         [Parameter("Sync Interval (sec)", Group = "Sync", DefaultValue = 10, MinValue = 5)]
         public int SyncIntervalSeconds { get; set; }
 
-        private const string BuildVersion = "v2026.05.21 18:14 - e1d62ae7";
+        private const string BuildVersion = "v2026.05.21 18:27 - 7ab7d41b";
 
         private string _serverStatus = "WAITING";
         private string _apiStatus = "WAITING";
