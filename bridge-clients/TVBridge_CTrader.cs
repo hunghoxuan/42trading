283bb48606a466a31171bd2bc9c5e38b05f6df6e
Hung Ho <classondev@gmail.com>
2026-05-21 17:01:39 +0200

    codex: chore bump build versions for chart snapshots fix deploy

--- bridge-clients/TVBridge_CTrader.cs
+++ bridge-clients/TVBridge_CTrader.cs
@@ -68,7 +68,7 @@ namespace cAlgo.Robots
         [Parameter("Sync Interval (sec)", Group = "Sync", DefaultValue = 10, MinValue = 5)]
         public int SyncIntervalSeconds { get; set; }
 
-        private const string BuildVersion = "v2026.05.21 14:30 - 1047429b";
+        private const string BuildVersion = "v2026.05.21 15:01 - 0f2eabf0";
 
         private string _serverStatus = "WAITING";
         private string _apiStatus = "WAITING";
